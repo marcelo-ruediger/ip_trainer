834f45fa87893d2792c04a139eacdc5c8b9fcb67
Manobrawn <161127661+Manobrawn@users.noreply.github.com>
2025-07-02 21:19:16 +0200

    fixed img link

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import tableImg from "./table.png";
+import tableImg from "../public/table.png";
 
 function App() {
     // ---------------------------- States ---------------------------//
