c890582a35fa50b56cc88f32ffecd1c67a16cfd7
Manobrawn <marceloruediger5@gmail.com>
2025-10-04 15:45:15 +0200

    little error on CSS datei fixed. Extra }

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "19e0d671c33973b9bc8f91a877defd5c"
   }, {
     "url": "index.html",
-    "revision": "0.7hb8jnombuo"
+    "revision": "0.ihbrsss7r7o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
