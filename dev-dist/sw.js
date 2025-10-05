84df0ce7639a38583f52c8c2992b93408ac01a57
Manobrawn <marceloruediger5@gmail.com>
2025-10-05 19:22:05 +0200

    Even more padding added to small screens because of the modals overlay

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "19e0d671c33973b9bc8f91a877defd5c"
   }, {
     "url": "index.html",
-    "revision": "0.oaomtbe0nq"
+    "revision": "0.2jna00pmhr"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
