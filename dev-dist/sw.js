946fcac383aa456acc9cd66b8475c02147075b1c
Manobrawn <marceloruediger5@gmail.com>
2025-10-06 17:49:59 +0200

    added some padding to the footer to make the footer elements easier to access

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "19e0d671c33973b9bc8f91a877defd5c"
   }, {
     "url": "index.html",
-    "revision": "0.9kgoqgg6log"
+    "revision": "0.8o9t2vumr1"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
