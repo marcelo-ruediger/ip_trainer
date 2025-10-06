0e371fde10369de18a715fdc42d5ca83cdc2de79
Manobrawn <marceloruediger5@gmail.com>
2025-10-06 17:31:40 +0200

    small fix to the footer. Increased the font-size and spacing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "19e0d671c33973b9bc8f91a877defd5c"
   }, {
     "url": "index.html",
-    "revision": "0.5j4e7gt9g1o"
+    "revision": "0.9kgoqgg6log"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
