1fdd7ec29f9852e6b5daa989c09eec66379eacdf
Manobrawn <marceloruediger5@gmail.com>
2025-10-06 13:42:24 +0200

    CSS rules added to fix hidden borders from layout Modals specially on Chrome

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "19e0d671c33973b9bc8f91a877defd5c"
   }, {
     "url": "index.html",
-    "revision": "0.sf7ooj9bv8"
+    "revision": "0.esfv0g8mnlo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
